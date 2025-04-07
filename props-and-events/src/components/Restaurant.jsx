4b528ef46841f78edf911868dfaf695bf9913970
Diana <Khachaturova.Diana@student.greenriver.edu>
2025-04-07 16:13:32 -0700

    Final touches, finished the last step of the Lab, and tested it successfully in the browser!

--- props-and-events/src/components/Restaurant.jsx
+++ props-and-events/src/components/Restaurant.jsx
@@ -1,7 +1,14 @@
-function Restaurant() {
+function Restaurant(props) {
   return (
     <div className="restaurant">
-  
+      <img src={props.restaurant.image} alt={props.restaurant.name} />
+      <h2>{props.restaurant.name}</h2>
+      <ul>
+        <li>{props.restaurant.address}</li>
+        <li>{props.restaurant.phone}</li>
+        <li>{props.restaurant.cuisine}</li>
+        <li>⭐ {props.restaurant.rating}</li>
+      </ul>
     </div>
   );
 }
